a6a42e8c62d69a9e6418a7e0576368fb52519642
David Busby <david.busby@quiq.com>
2023-06-02 15:53:53 +0100

    Diagram updates

--- diagram.py
+++ diagram.py
@@ -15,8 +15,13 @@ with Diagram("Internal infra Diagram as Code"):
         unifi   = Custom("unifi-controller", "icons/unifi.png")
 
     with Cluster("internet-pi2.int.oneiroi.co.uk - NOMAD"):
-        cf_argo2 = Custom("cloudflared", "icons/cf-logo-v-rgb.png")
+        cf_argo2 = Custom("cloudflared2", "icons/cf-logo-v-rgb.png")
         pihole2  = Custom("pi.hole2", "icons/pihole.png")
+        #unifi   = Custom("unifi")
+
+    with Cluster("internet-pi3.int.oneiroi.co.uk - NOMAD"):
+        cf_argo3 = Custom("cloudflared3", "icons/cf-logo-v-rgb.png")
+        pihole3  = Custom("pi.hole3", "icons/pihole.png")
         #unifi   = Custom("unifi")
 
     #usg connection to unfi controller, bi-driectional, cfg management and reporting
@@ -29,11 +34,17 @@ with Diagram("Internal infra Diagram as Code"):
     #Traffic flow AP to USG 
     ap >> usg << aps
 
-    #pihole-1 DNSoH query to cf_argo, secondary direct to cf_argo2
-    pihole >> Edge(label="DNSoH") >> cf_argo
-    pihole >> Edge(label="DNSoH") >> cf_argo2
+    #pihole1 DNSoH query to cf_argo{..3}
+    pihole >> Edge(label="DNS over HTTPS") >> cf_argo
+    pihole >> Edge(label="DNS over HTTPS") >> cf_argo2
+    pihole >> Edge(label="DNS over HTTPS") >> cf_argo3
      
-    #pihole-3 DNSoH query to cf_argoi2, secondary direct to cf_argo
-    pihole2 >> Edge(label="DNS over HTTPS") >> cf_argo2
+    #pihole2 DNSoH query to cf_argo{..3}
     pihole2 >> Edge(label="DNS over HTTPS") >> cf_argo
+    pihole2 >> Edge(label="DNS over HTTPS") >> cf_argo2
+    pihole2 >> Edge(label="DNS over HTTPS") >> cf_argo3
 
+    #pihole3 DNSoH query to cf_argo{..3}
+    pihole3 >> Edge(label="DNS over HTTPS") >> cf_argo
+    pihole3 >> Edge(label="DNS over HTTPS") >> cf_argo2
+    pihole3 >> Edge(label="DNS over HTTPS") >> cf_argo3
